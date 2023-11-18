8f2b20faff2b1a70d16284757fae1ed6e65f142a
matin <matinsadegi3290@gmail.com>
2023-11-18 16:11:53 +0330

    add new icons

--- services/productServices.tsx
+++ services/productServices.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FormData } from "@/types/types";
+import { AttributesData, FormData } from "@/types/types";
 
 export async function createProduct(newProduct: FormData) {
   await axios.post("http://localhost:5000/api/product", newProduct);
@@ -8,4 +8,26 @@ export async function createProduct(newProduct: FormData) {
 export async function getProducts() {
   const { data } = await axios.get("http://localhost:5000/api/product");
   return data;
+}
+export async function getProduct(slug: string) {
+  const { data } = await axios.get(`http://localhost:5000/api/product/${slug}`);
+  return data;
+}
+
+export async function deleteProduct(slug: string) {
+  await axios.delete(`http://localhost:5000/api/product/${slug}`);
+}
+
+export async function addAttribute(attribute: AttributesData) {
+  await axios.post("http://localhost:5000/api/attributes", attribute);
+}
+
+export async function getAttributes() {
+  const { data } = await axios.get("http://localhost:5000/api/attributes");
+  return data;
+}
+
+export async function deleteAttribute(name: string) {
+  const response =await axios.delete(`http://localhost:5000/api/attributes/${name}`);
+  return response
 }
